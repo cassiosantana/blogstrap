2379f2df578f5664878b42d4bc1961b72de852bd
Cassio Santana <cassioo.roger@gmail.com>
2023-04-11 12:25:21 -0300

    feat: Redefinida rota da página index
    Foi redefinida a página inicial da aplicação
    para a página index de da rota /articles
    
    Ref #4

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # definindo a página inicial do rails como a index sem precisar ficar digitando manualmente o localhost:3000/articles
+  root 'articles#index'
   #articles#index significa: controller articles na action index
   get '/articles', to: 'articles#index'
 end
