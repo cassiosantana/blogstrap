7add8ba80d97c9442a1c9fb3f4ca43112a09c3b9
Cassio Santana <cassioo.roger@gmail.com>
2023-04-23 10:24:51 -0300

    build(aula06): Adicionado validações ao Article
    Validações adicionadas ao model Article que impedem
    ausência de título, corpo e valores mínimos de dígitos.
    
    Ref #6

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,7 @@
 class Article < ApplicationRecord
+  # validações para o title e body que para o artigo ser salvo
+  # é necessário que estes campos estejam preenchidos e com suas
+  # quantidades de caracteres mínimos
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :body, presence: true, length: { minimum: 10 }
 end
