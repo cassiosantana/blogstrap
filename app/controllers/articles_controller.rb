18bf6829c5c5e57b66d8b0d394a41cb8ec57a5ea
Cassio Santana <cassioo.roger@gmail.com>
2023-04-25 09:24:47 -0300

    refactor(aula08): Modificada action show
    Ref #8

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,9 +8,7 @@ class ArticlesController < ApplicationController
     @articles = Article.all
   end
 
-  def show
-    @article = Article.find(params[:id])
-  end
+  def show; end
 
   def new
     @article = Article.new
