92900bd9f7668c3c886e2ff179d247f33c651316
Cassio Santana <cassioo.roger@gmail.com>
2023-05-03 12:11:08 -0300

    feat(aula10.2): Adiciona comentário
    Ref #12

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,7 +6,8 @@ class ArticlesController < ApplicationController
 
   def index
     current_page = (params[:page] || 1).to_i
-    @articles = Article.page(current_page).per(2)
+
+    @articles = Article.order(created_at: :desc).page(current_page).per(2)
   end
 
   def show; end
