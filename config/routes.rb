64442a7ca5817694db869b6720999f3057789cb4
Cassio Santana <cassioo.roger@gmail.com>
2023-05-08 08:53:31 -0300

    build(aula13): Adiciona scaffold categories
    Ref #24

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :categories
   # definindo a página inicial do rails como a index sem precisar ficar digitando manualmente o localhost:3000/articles
   root 'articles#index'
   #articles#index significa: controller articles na action index
