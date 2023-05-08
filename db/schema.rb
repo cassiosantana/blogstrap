f80b2d8d37f2c38f9a3eb4b2caf7b1f42edb0955
Cassio Santana <cassioo.roger@gmail.com>
2023-05-08 09:29:22 -0300

    build(aula13): Realiza migration
    Adiciona referência de category aos article
    
    Ref #24

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_08_114940) do
+ActiveRecord::Schema.define(version: 2023_05_08_120504) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id", null: false
+    t.index ["category_id"], name: "index_articles_on_category_id"
   end
 
   create_table "categories", force: :cascade do |t|
@@ -25,4 +27,5 @@ ActiveRecord::Schema.define(version: 2023_05_08_114940) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "articles", "categories"
 end
