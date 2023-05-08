6e08cdc0b2ec7f00a515daf691b6d0f2c9d33f73
Cassio Santana <cassioo.roger@gmail.com>
2023-05-08 08:57:17 -0300

    build(aula13): Realiza o migration de categories
    Ref #24

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_11_153127) do
+ActiveRecord::Schema.define(version: 2023_05_08_114940) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
