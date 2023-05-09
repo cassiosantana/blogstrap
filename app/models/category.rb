cad842b1a62bb8912413d3c462d78f8a9d48311c
Cassio Santana <cassioo.roger@gmail.com>
2023-05-09 11:11:44 -0300

    style(aula13): Ordena lista de categorias index categories Ref #24

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,6 @@ class Category < ApplicationRecord
   # define que uma categoria possui vários artigos
   has_many :articles
   validates :name, presence: true, length: { minimum: 3 }
+
+  scope :sorted, -> { order(:name) }
 end
