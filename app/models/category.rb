71599714137ffca38a123d29c0e912d81ef8b13b
Cassio Santana <cassioo.roger@gmail.com>
2023-05-10 09:33:00 -0300

    build(aula13): Adiciona erro de dependência ao Category
    Ref #24

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ApplicationRecord
   # define que uma categoria possui vários artigos
-  has_many :articles
+  has_many :articles, dependent: :restrict_with_error
   validates :name, presence: true, length: { minimum: 3 }
 
   scope :sorted, -> { order(:name) }
