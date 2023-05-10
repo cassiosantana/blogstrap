8c156f437aa4f21c8c05a3ace5fbd7d98559bb4b
Cassio Santana <cassioo.roger@gmail.com>
2023-05-10 10:45:03 -0300

    build(aula13): Exclui o recurso show
    Closes #24

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :categories
   # definindo a página inicial do rails como a index sem precisar ficar digitando manualmente o localhost:3000/articles
   root 'articles#index'
-  #articles#index significa: controller articles na action index
   # get '/articles', to: 'articles#index'
 
+  # articles#index significa: controller articles na action index
   # get '/articles/:id', to: 'articles#show'
 
   resources :articles
+  resources :categories, except: [:show]
 end
