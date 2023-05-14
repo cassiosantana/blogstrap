372a4476e3a48372d86f480015eefbf22be3fc7f
Cassio Santana <cassioo.roger@gmail.com>
2023-05-14 11:11:43 -0300

    doc(aula15): Adiciona comentário a regra de autorização
    Ref #26

--- app/policies/article_policy.rb
+++ app/policies/article_policy.rb
@@ -9,6 +9,7 @@ class ArticlePolicy < ApplicationPolicy
     end
 
     def update?
+      # verificar se o usuário atual tem o mesmo id do usuário que criou o record(article)
       user.id == record.user.id
     end
 
