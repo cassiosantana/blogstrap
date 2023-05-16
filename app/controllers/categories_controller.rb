590d667f9ad14e89d1a2245e78cb8ded047f6e01
Cassio Santana <cassioo.roger@gmail.com>
2023-05-16 12:35:50 -0300

    style(aula15): Corrige indentação
    Ref #26

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -60,13 +60,13 @@ class CategoriesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_category
-      @category = Category.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_category
+    @category = Category.find(params[:id])
+  end
 
-    # Only allow a list of trusted parameters through.
-    def category_params
-      params.require(:category).permit(:name)
-    end
+  # Only allow a list of trusted parameters through.
+  def category_params
+    params.require(:category).permit(:name)
+  end
 end
