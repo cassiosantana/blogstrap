2fee1176e3b5b88e92746c22ecd9db5a62427f06
Cassio Santana <cassioo.roger@gmail.com>
2023-05-21 08:00:35 -0300

    feat(aula18): Adiciona filtro por mês
    Ref #40

--- app/models/article.rb
+++ app/models/article.rb
@@ -16,4 +16,10 @@ class Article < ApplicationRecord
   scope :descending_order, -> { order(created_at: :desc) }
   scope :without_highlights, ->(ids) { where("id NOT IN(#{ids})") if ids.present? }
   scope :filter_by_category, ->(category) { where category_id: category.id if category.present? }
+  scope :filter_by_archive, lambda { |month_year|
+    if month_year
+      date = Date.strptime(month_year, '%B %Y')
+      where created_at: date.beginning_of_month..date.end_of_month
+    end
+  }
 end
