3b8778a6fa7d71845fddec7d94e71b2b09b0485a
Cassio Santana <cassioo.roger@gmail.com>
2023-06-05 07:02:51 -0300

    build(aula20): Adiciona validação de senha forte
    Ref #42

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,15 @@ class User < ApplicationRecord
          :rememberable,
          :trackable,
          :validatable
+
+  validate :password_complexity
+
+  private
+
+  def password_complexity
+    # Regexp extracted from https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
+    return if password.blank? || password =~ /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/
+
+    errors.add :password, 'Complexity requirement not met. Please use: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+  end
 end
