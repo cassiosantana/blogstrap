5fdf8e1c7d5c4bcb4d111e2841edf9f6a66e3ae6
Cassio Santana <cassioo.roger@gmail.com>
2023-06-06 08:28:29 -0300

    refactor(aula20): Altera código da validação de complexidade de senha
    Agora utiliza os serviços de complexidade de senha.
    
    Ref #42

--- app/models/user.rb
+++ app/models/user.rb
@@ -18,9 +18,8 @@ class User < ApplicationRecord
   private
 
   def password_complexity
-    # Regexp extracted from https://stackoverflow.com/questions/19605150/regex-for-password-must-contain-at-least-eight-characters-at-least-one-number-a
-    return if password.blank? || password =~ /(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-])/
+    return if password.nil?
 
-    errors.add :password, 'Complexity requirement not met. Please use: 1 uppercase, 1 lowercase, 1 digit and 1 special character'
+    errors.add :password, :complexity unless CheckPasswordComplexityService.call(password)
   end
 end
